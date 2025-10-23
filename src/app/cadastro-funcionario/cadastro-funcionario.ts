b5a9fe7e6d1946ab8a3f37abee6bfec2d877ed3e
Bruno Barros <125375715+toyomori-dev@users.noreply.github.com>
2025-10-22 22:10:02 -0300

    Add files via upload

--- src/app/cadastro-funcionario/cadastro-funcionario.ts
+++ src/app/cadastro-funcionario/cadastro-funcionario.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { Funcionario } from '../services/funcionario';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-funcionario',
   standalone: false,
   templateUrl: './cadastro-funcionario.html',
-  styleUrl: './cadastro-funcionario.css',
+  styleUrls: ['./cadastro-funcionario.css'] 
 })
 export class CadastroFuncionario {
 
- constructor(private funcionariosService: Funcionario, private router: Router) {}
+  constructor(
+    private funcionariosService: Funcionario,
+    private router: Router
+  ) {}
 
-    formFuncionario = new FormGroup({
+  formFuncionario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
     sobrenome: new FormControl('', [Validators.required, Validators.minLength(3)]),
     sexo: new FormControl('', Validators.required),
@@ -22,18 +25,34 @@ export class CadastroFuncionario {
     endereco: new FormControl('', Validators.required),
     foto: new FormControl('', Validators.required),
     valorPassagem: new FormControl('', Validators.required),
-    optouVT: new FormControl('', Validators.required),
+    optouVT: new FormControl(false), 
     cargo: new FormControl('', Validators.required),
     salario: new FormControl('', Validators.required),
     dataInicio: new FormControl('', Validators.required),
     dataDemissao: new FormControl(null),
-    });
+  });
 
-    enviar(){
-      this.funcionariosService.postFuncionarios(this.formFuncionario.value).subscribe((data) => {
-        console.log(data);
-      })
-      this.router.navigate(['/lista-funcionarios']);
+  enviar() {
+    if (this.formFuncionario.invalid) {
+      alert('Preencha todos os campos corretamente antes de enviar!');
+      this.formFuncionario.markAllAsTouched();
+      return;
     }
 
+    this.funcionariosService.postFuncionarios(this.formFuncionario.value).subscribe({
+      next: (data) => {
+        console.log('Funcionário cadastrado:', data);
+        alert('Funcionário cadastrado com sucesso!');
+
+        this.formFuncionario.reset();
+
+        
+        this.router.navigate(['/lista-funcionarios']);
+      },
+      error: (err) => {
+        console.error('Erro ao cadastrar funcionário:', err);
+        alert('Ocorreu um erro ao cadastrar. Tente novamente.');
+      }
+    });
+  }
 }
--- src/app/lista-funcionarios/lista-funcionarios.ts
+++ src/app/lista-funcionarios/lista-funcionarios.ts
@@ -1,32 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Funcionario } from '../services/funcionario';
-import {MatTableModule} from '@angular/material/table';
-
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-lista-funcionarios',
   standalone: false,
   templateUrl: './lista-funcionarios.html',
-  styleUrl: './lista-funcionarios.css'
+  styleUrl: './lista-funcionarios.css',
 })
-
-
 export class ListaFuncionarios implements OnInit {
   funcionariosLista: any[] = [];
   displayedColumns: string[] = ['nome', 'sobrenome', 'dtNascimento', 'salario', 'detalhes'];
   dataSource = [];
 
-
-  constructor(private funcionario: Funcionario, private router: Router) {
-  }
+  constructor(
+    private funcionario: Funcionario,
+    private router: Router,
+    private route: ActivatedRoute,
+    private funcionarioService: Funcionario
+  ) {}
 
   ngOnInit(): void {
     this.getFuncionarios();
+    this.route.paramMap.subscribe((params) => {
+      this.idUsuario = params.get('id');
+      this.getUserDetails(this.idUsuario);
+    });
   }
 
   funcionarios: any[] = [];
+  idUsuario: any = '';
+  usuario: any;
 
+  getUserDetails(id: string) {
+    return this.funcionarioService.getUserById(id).subscribe((data: any) => {
+      this.usuario = data;
+    });
+  }
   getFuncionarios() {
     this.funcionario.getFuncionarios().subscribe((data: any) => {
       console.log(data);
@@ -43,7 +54,5 @@ export class ListaFuncionarios implements OnInit {
     this.router.navigate(['/cadastrar']);
   }
 
-
-
-
+  
 }
